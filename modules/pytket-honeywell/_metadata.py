54c49df52b398afea26ac3f875490bdf77900a27
Luca Mondada <luca.mondada@cqc.com>
2021-02-25 13:53:10 +0000

    bumped version numbers

--- modules/pytket-honeywell/_metadata.py
+++ modules/pytket-honeywell/_metadata.py
@@ -1,2 +1,2 @@
-__extension_version__ = "0.6.2"
+__extension_version__ = "0.6.3"
 __extension_name__ = "pytket-honeywell"
--- modules/pytket-pyzx/_metadata.py
+++ modules/pytket-pyzx/_metadata.py
@@ -1,2 +1,2 @@
-__extension_version__ = "0.6.0"
+__extension_version__ = "0.6.1"
 __extension_name__ = "pytket-pyzx"
