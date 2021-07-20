43cebd42e4b6bf13c3f3b80038f1882ba5eb7da1
sjdilkes <silas.dilkes@cambridgequantum.com>
2021-07-20 14:56:34 +0100

    update name , cast types

--- modules/pytket-qiskit/pytket/extensions/qiskit/backends/aer.py
+++ modules/pytket-qiskit/pytket/extensions/qiskit/backends/aer.py
@@ -440,8 +440,12 @@ class AerBackend(_AerBaseBackend):
             self._backend_info.all_edge_gate_errors = characterisation["EdgeErrors"]
             self._backend_info.all_readout_errors = characterisation["ReadoutErrors"]
 
-            self._backend_info.averaged_node_errors = averaged_errors["node_errors"]
-            self._backend_info.averaged_edge_errors = averaged_errors["edge_errors"]
+            self._backend_info.averaged_node_gate_errors = averaged_errors[
+                "node_errors"
+            ]
+            self._backend_info.averaged_edge_gate_errors = averaged_errors[
+                "edge_errors"
+            ]
             self._backend_info.averaged_readout_errors = averaged_errors[
                 "readout_errors"
             ]
--- modules/pytket-qiskit/tests/backend_test.py
+++ modules/pytket-qiskit/tests/backend_test.py
@@ -306,14 +306,16 @@ def test_process_characterisation_complete_noise_model() -> None:
     link_errors = back.backend_info.all_edge_gate_errors
     arch = back.backend_info.architecture
 
-    assert char["GenericTwoQubitQErrors"][(0, 1)][0][1][0] == 0.0375
-    assert char["GenericTwoQubitQErrors"][(0, 1)][0][1][15] == 0.4375
-    assert char["GenericOneQubitQErrors"][0][0][1][0] == 0.125
-    assert char["GenericOneQubitQErrors"][0][0][1][3] == 0.625
-    assert char["GenericOneQubitQErrors"][0][1][1][0] == 0.35
-    assert char["GenericOneQubitQErrors"][0][1][1][1] == 0.65
-    assert char["GenericOneQubitQErrors"][0][2][1][0] == 0.35
-    assert char["GenericOneQubitQErrors"][0][2][1][1] == 0.65
+    gqe2 = cast(Dict, char["GenericTwoQubitQErrors"])
+    gqe1 = cast(Dict, char["GenericOneQubitQErrors"])
+    assert gqe2[(0, 1)][0][1][0] == 0.0375
+    assert gqe2[(0, 1)][0][1][15] == 0.4375
+    assert gqe1[0][0][1][0] == 0.125
+    assert gqe1[0][0][1][3] == 0.625
+    assert gqe1[0][1][1][0] == 0.35
+    assert gqe1[0][1][1][1] == 0.65
+    assert gqe1[0][2][1][0] == 0.35
+    assert gqe1[0][2][1][1] == 0.65
     assert node_errors[arch.nodes[0]][OpType.U3] == 0.375
     assert link_errors[(arch.nodes[0], arch.nodes[1])][OpType.CX] == 0.5625
     assert link_errors[(arch.nodes[1], arch.nodes[0])][OpType.CX] == 0.80859375
