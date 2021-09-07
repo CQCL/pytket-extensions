b1e103702b4561c3a7773e36c0778716c8d175fa
melf <cqc@melf.de>
2021-09-07 13:02:38 +0100

    fix qsharp version

--- modules/pytket-qsharp/setup.py
+++ modules/pytket-qsharp/setup.py
@@ -40,7 +40,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "pytket == 0.14.0",
-        "qsharp ~=0.18.2107",
+        "qsharp ==0.18.2108160999",
         "qsharp-core ~=0.18.2107",
     ],
     classifiers=[
