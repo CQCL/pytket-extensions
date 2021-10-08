311026059654a68efd827b2a5ebe233683435a7f
Alec Edgington <alec.edgington@cambridgequantum.com>
2021-10-08 09:25:55 +0100

    Fix references.

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ html_theme = "sphinx_rtd_theme"
 
 # -- Extension configuration -------------------------------------------------
 
-pytketdoc_base = "https://cqcl.github.io/pytket/build/html/"
+pytketdoc_base = "https://cqcl.github.io/tket/pytket/doc/html/"
 
 intersphinx_mapping = {
     "https://docs.python.org/3/": None,
