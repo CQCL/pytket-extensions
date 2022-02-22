64e51b3403ed7cb784b515b9ebbed32417532670
sjdilkes <silas.dilkes@cambridgequantum.com>
2022-02-22 08:36:18 +0000

    update pytket-honeywell for pytket1.0.0rc11

--- modules/pytket-honeywell/setup.py
+++ modules/pytket-honeywell/setup.py
@@ -38,7 +38,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket == 1.0.0rc9",
+        "pytket == 1.0.0rc11",
         "requests >= 2.2",
         "types-requests",
         "websockets >= 7.0",
