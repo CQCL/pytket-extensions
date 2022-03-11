d2ca423558d92218b1385ddd6bc65e095d36a868
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2022-03-11 15:02:07 +0000

    [infra] Update to pytket 1.0.0rc18. (#334)

--- modules/pytket-qsharp/setup.py
+++ modules/pytket-qsharp/setup.py
@@ -39,7 +39,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket == 1.0.0rc17",
+        "pytket == 1.0.0rc18",
         "qsharp ~= 0.23.195983",
         "qsharp-core ~= 0.23.195983",
     ],
