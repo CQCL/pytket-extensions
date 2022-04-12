ce0e45e85f590ac0ee71da62786f25a1dccc45e2
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-04-12 10:56:38 +0100

    Require pytket 1.1.

--- modules/pytket-cirq/setup.py
+++ modules/pytket-cirq/setup.py
@@ -39,7 +39,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.0",
+        "pytket ~= 1.1",
         "cirq-core ~= 0.14.0",
         "cirq-google ~= 0.14.0",
     ],
--- modules/pytket-pysimplex/setup.py
+++ modules/pytket-pysimplex/setup.py
@@ -38,7 +38,7 @@ setup(
     license="Apache 2",
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
-    install_requires=["pytket ~= 1.0", "pysimplex ~= 1.4"],
+    install_requires=["pytket ~= 1.1", "pysimplex ~= 1.4"],
     classifiers=[
         "Environment :: Console",
         "Programming Language :: Python :: 3.8",
