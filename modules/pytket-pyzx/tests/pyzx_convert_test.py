01a193b3bfeca93ba3620f1d1a9b11e43b2eecae
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2022-06-06 10:03:37 +0200

    [pyzx] add pyzx arc conversion (#393)
    * add pyzx architecture conversion
    
    * rename pytket to tk in functionnames
    
    * rename pyzxarc to PyzxArc
    
    * rename pyzxgraph to PyzxGraph
    
    * replace in the node set of .
    
    * add parameter for src name in pyzx
    
    * return directly in pyzx_to_tk_arc
    
    * add usage of denominator_limit
    
    * remove >>all PyZX basic gate operations are currently supported by pytket.<<
    
    * add more information about the map in the documentation
    
    * add more information to the doc about the map parameter
    
    * add check for circuit equality
    
    * clean up

--- modules/pytket-pyzx/tests/pyzx_convert_test.py
+++ modules/pytket-pyzx/tests/pyzx_convert_test.py
@@ -12,9 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pytket.extensions.pyzx import tk_to_pyzx, pyzx_to_tk
+from pytket.extensions.pyzx import (
+    tk_to_pyzx,
+    pyzx_to_tk,
+    tk_to_pyzx_arc,
+    pyzx_to_tk_arc,
+    tk_to_pyzx_placed_circ,
+    pyzx_to_tk_placed_circ,
+)
 from pytket.circuit import Circuit, fresh_symbol  # type: ignore
-
+from pytket.architecture import Architecture  # type: ignore
+from pytket.passes import AASRouting, CXMappingPass  # type: ignore
+from pytket.placement import GraphPlacement  # type: ignore
 import numpy as np
 import pytest
 
@@ -60,3 +69,74 @@ def test_invalid_gate() -> None:
     with pytest.raises(Exception) as excinfo:
         _ = tk_to_pyzx(circ)
         assert "as the gate type is unrecognised." in str(excinfo.value)
+
+
+@pytest.mark.filterwarnings("ignore:strict=False")
+def test_arc_conversion() -> None:
+    arc = Architecture([[0, 1], [1, 2], [2, 3], [3, 4]])
+    arc_pyzx = tk_to_pyzx_arc(arc)
+    arc_2 = pyzx_to_tk_arc(arc_pyzx)
+    assert arc == arc_2
+    arc_pyzx_2 = tk_to_pyzx_arc(arc_2)
+    assert list(arc_pyzx.graph.edges()) == list(arc_pyzx_2.graph.edges())
+    assert list(arc_pyzx.graph.vertices()) == list(arc_pyzx_2.graph.vertices())
+
+
+@pytest.mark.filterwarnings("ignore:strict=False")
+def test_placed_circ_tests() -> None:
+    arc = Architecture([[0, 2], [1, 2], [2, 3], [3, 4]])
+    initial_circ = Circuit(5)
+    initial_circ.H(0)
+    initial_circ.H(1)
+    initial_circ.H(2)
+    initial_circ.H(3)
+    initial_circ.H(4)
+    initial_circ.CX(3, 4)
+    c = initial_circ.copy()
+
+    aas_pass = AASRouting(arc)
+
+    aas_pass.apply(c)
+
+    _, pyzx_circ, inv_map = tk_to_pyzx_placed_circ(c, arc)
+
+    pytket_circ_2 = pyzx_to_tk_placed_circ(pyzx_circ, inv_map)
+
+    assert pytket_circ_2.qubits == c.qubits
+
+    state = c.get_statevector()
+    state2 = pytket_circ_2.get_statevector()
+    assert np.allclose(state, state2, atol=1e-10)
+
+
+@pytest.mark.filterwarnings("ignore:strict=False")
+def test_placed_circ_tests_2() -> None:
+    arc = Architecture([[0, 2], [1, 2], [2, 3], [3, 4]])
+    initial_circ = Circuit(5)
+    initial_circ.H(0)
+    initial_circ.H(1)
+    initial_circ.H(2)
+    initial_circ.H(3)
+    initial_circ.H(4)
+    initial_circ.CX(1, 4)
+    initial_circ.CX(4, 2)
+    initial_circ.CX(0, 4)
+    initial_circ.CX(1, 4)
+
+    c = initial_circ.copy()
+
+    g_place = GraphPlacement(arc)
+
+    cx_pass = CXMappingPass(arc, g_place)
+
+    cx_pass.apply(c)
+
+    _, pyzx_circ, inv_map = tk_to_pyzx_placed_circ(c, arc)
+
+    pytket_circ_2 = pyzx_to_tk_placed_circ(pyzx_circ, inv_map)
+
+    assert pytket_circ_2.qubits == c.qubits
+
+    state = c.get_statevector()
+    state2 = pytket_circ_2.get_statevector()
+    assert np.allclose(state, state2, atol=1e-10)
