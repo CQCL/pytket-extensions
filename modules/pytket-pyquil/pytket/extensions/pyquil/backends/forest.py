709c5cf207c15acaf0b977474eb680a7dff71d90
melf <cqc@melf.de>
2022-06-23 14:52:00 +0100

    add info to rigetti device

--- modules/pytket-pyquil/pytket/extensions/pyquil/backends/forest.py
+++ modules/pytket-pyquil/pytket/extensions/pyquil/backends/forest.py
@@ -100,6 +100,9 @@ def _default_q_index(q: Qubit) -> int:
 
 
 class ForestBackend(Backend):
+    """
+    Interface to a Rigetti device.
+    """
     _supports_shots = True
     _supports_counts = True
     _supports_contextual_optimisation = True
@@ -303,6 +306,9 @@ class ForestBackend(Backend):
 
 
 class ForestStateBackend(Backend):
+    """
+    State based interface to a Rigetti device.
+    """
     _supports_state = True
     _supports_expectation = True
     _expectation_allows_nonhermitian = False
