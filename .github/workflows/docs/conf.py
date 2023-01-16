ef8962b87943843d711e9a62ffa87f33b839b108
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2023-01-16 17:13:14 +0000

    rename logo

--- .github/workflows/docs/conf.py
+++ .github/workflows/docs/conf.py
@@ -26,7 +26,7 @@ html_static_path = ["_static"]
 
 html_css_files = ["custom.css"]
 
-html_logo = "_static/Quantinuum_logo2.png"
+html_logo = "_static/Quantinuum_logo.png"
 
 # -- Extension configuration -------------------------------------------------
 
